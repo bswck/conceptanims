10ae8858197e019bfb55596ea01c2edf2050922d
bswck <bswck.dev@gmail.com>
2023-04-10 15:46:42 +0200

    Add square->rectangle animation

--- conceptanims/geometry/areas.py
+++ conceptanims/geometry/areas.py
@@ -4,7 +4,8 @@ from manim import *
 FONT_SIZE = 24
 FIGURE_COLOR = YELLOW_C
 FILL_COLOR = YELLOW_C
-CHAR_COLOR = YELLOW_E
+CHAR_COLOR = YELLOW_D
+CHAR_COLOR_SPECIAL = YELLOW_E
 
 
 class Areas(Scene):
@@ -23,12 +24,12 @@ class Areas(Scene):
         area_formula.set_color_by_tex('?', CHAR_COLOR)
 
         self.play(FadeIn(point, char, area_formula))
-        self.wait(2)
-        self.play(FadeOut(area_formula))
+        self.wait()
+        self.play(Indicate(area_formula))
 
         line = Line(LEFT, RIGHT, color=FIGURE_COLOR)
         new_char = MathTex('a', color=CHAR_COLOR).next_to(line, UP)
-        area_formula = MathTex('P =', 'a').shift(DOWN)
+        line_area_formula = MathTex('P =', 'a').shift(DOWN)
         area_formula.set_color_by_tex('a', CHAR_COLOR)
 
         lpoint, rpoint = point, point.copy()
@@ -40,10 +41,10 @@ class Areas(Scene):
         self.play(
             Create(line),
             char.animate.become(new_char),
-            FadeIn(area_formula)
+            area_formula.animate.become(line_area_formula)
         )
-        self.wait(2.5)
-        self.play(FadeOut(area_formula))
+        self.wait()
+        self.play(Indicate(area_formula))
 
         char.add_updater(lambda m, _=None: m.next_to(line, UP), call_updater=True)
         lpoint.add_updater(lambda m, _=None: m.move_to(line.get_start()), call_updater=True)
@@ -66,38 +67,31 @@ class Areas(Scene):
         fill = VMobject()
         fill.add_updater(lambda m, _=None: m.become(Polygon(
             lpoint.get_center(), rpoint.get_center(), rside.get_end(), lside.get_end(),
-            stroke_width=0, fill_color=FILL_COLOR, fill_opacity=0.5
+            stroke_width=0, fill_color=FILL_COLOR, fill_opacity=0.7
         )))
 
         self.play(line.animate.shift(UP*2))
 
-        area_formula = MathTex('P =', 'a', r'\cdot', '?').shift(DOWN)
-        area_formula.set_color_by_tex('a', CHAR_COLOR)
-        area_formula.set_color_by_tex('?', GREY)
-        self.play(FadeIn(area_formula))
-        self.add(bottom_lpoint, bottom_rpoint, bottom_line, side_char, fill)
+        square_area_formula = MathTex('P =', 'a', r'\cdot', '?').shift(DOWN)
+        square_area_formula.set_color_by_tex('a', CHAR_COLOR)
+        square_area_formula.set_color_by_tex('?', GREY)
+        self.add(bottom_lpoint, bottom_rpoint, bottom_line, fill)
 
         side_creation_runtime = 4
         self.play(
+            FadeIn(side_char, lag_ratio=0.01),
+            area_formula.animate.become(square_area_formula),
             Create(lside, run_time=side_creation_runtime),
             Create(rside, run_time=side_creation_runtime),
         )
 
-        comparison_line = Line(line.get_end(), line.get_start(), color=FIGURE_COLOR)
-        comparison_line_end = lpoint.copy()
-        comparison_line_end.add_updater(lambda m, _=None: m.move_to(comparison_line.get_end()))
-        self.add(comparison_line_end)
+        comparison_line = line.copy().set_color(WHITE)
+        self.play(Create(comparison_line))
         self.play(
-            Rotate(
-                comparison_line, PI/2,
-                about_point=line.get_end(),
-                rate_func=linear,
-                run_time=1.2
-            )
+            comparison_line.animate.become(rside.copy().set_color(WHITE)),
         )
+        self.play(Uncreate(comparison_line))
         self.play(
-            Circumscribe(line, color=GREY),
-            Circumscribe(comparison_line, color=GREY),
             side_char.animate.become(
                 MathTex('a', color=CHAR_COLOR).next_to(rside.get_center(), RIGHT),
             ),
@@ -105,14 +99,14 @@ class Areas(Scene):
                 MathTex('P =', 'a', r'\cdot', 'a').shift(DOWN).set_color_by_tex('a', CHAR_COLOR)
             )
         )
-        self.remove(comparison_line, comparison_line_end)
 
         self.wait()
+        self.play(Indicate(area_formula))
 
-        square = Square(color=FIGURE_COLOR, fill_color=FILL_COLOR, fill_opacity=0.5).shift(UP)
+        square = Square(color=FIGURE_COLOR, fill_color=FILL_COLOR, fill_opacity=0.7).shift(UP)
 
         self.add(square)
-        self.remove(lside, rside, bottom_line, fill, line)
+        self.remove(comparison_line, lside, rside, bottom_line, fill, line)
         self.play(
             FadeOut(
                 lpoint, rpoint,
@@ -132,6 +126,53 @@ class Areas(Scene):
         self.play(
             FadeIn(side_char, lag_ratio=0.01, run_time=0.4),
         )
-        self.play(
-            square.animate.scale(0.5).shift(UP+LEFT*2),
+        self.play(square.animate.scale(0.5).shift(LEFT*0.5))
+        duplicate_square = square.copy()
+        duplicate_char = char.copy()
+        duplicate_char.clear_updaters()
+        duplicate_char.add_updater(
+            lambda m, _=None: m.next_to(duplicate_square.get_top(), UP), call_updater=True
         )
+
+        self.add(duplicate_char)
+        self.play(
+            char.animate.set_color(CHAR_COLOR_SPECIAL),
+            duplicate_char.animate.set_color(CHAR_COLOR_SPECIAL),
+            duplicate_square.animate.shift(RIGHT),
+            area_formula.animate.become(
+                MathTex(
+                    'P =', 'a', r'\cdot', '(a+a)'
+                ).shift(DOWN).set_color_by_tex('(a+a)', CHAR_COLOR_SPECIAL)
+            )
+        )
+
+        square.side_length = square.width
+        rectangle = Rectangle(
+            height=square.side_length,
+            width=square.side_length*2,
+            color=FIGURE_COLOR,
+            fill_color=FILL_COLOR,
+            fill_opacity=0.7
+        ).shift(UP)
+        old_char, char = char, MathTex('b', color=CHAR_COLOR_SPECIAL).next_to(rectangle.get_top(), UP)
+
+        self.play(
+            old_char.animate.move_to(char),
+            duplicate_char.animate.move_to(char),
+            FadeOut(square, duplicate_square, old_char, duplicate_char),
+            FadeIn(rectangle, char),
+            LaggedStart(
+                area_formula.animate.become(
+                    MathTex(
+                        'P =', 'a', r'\cdot', 'b',
+                    ).shift(DOWN)
+                    .set_color_by_tex('a', CHAR_COLOR)
+                    .set_color_by_tex('b', CHAR_COLOR_SPECIAL)
+                ),
+                lag_ratio=0.1,
+            )
+        )
+        self.remove(old_char, duplicate_char)
+        self.add(char)
+        self.wait()
+        self.play(Indicate(area_formula))
